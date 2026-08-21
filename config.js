9083e5b5debe3f8bb99fe864c441379bf8dee656
smjanz <sara_janz@hotmail.com>
2026-08-21 17:49:23 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-window.SUPABASE_URL = "https://eyrjommxtsjlehcgxxah.supabase.co/rest/v1/";
+window.SUPABASE_URL = "https://eyrjommxtsjlehcgxxah.supabase.co";
 window.SUPABASE_PUBLISHABLE_KEY = "sb_publishable_QOwTOWdN1GNZzY6-Ju6kGg_cxElpFgm";
